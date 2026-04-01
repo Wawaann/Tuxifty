792990547426f1f0d35d535bd2176632fad19282
Wawaann <bigotte.ewan@gmail.com>
2026-04-01 15:12:36 +0200

    Initial Commit

---
+++ Tuxifty/TuxiftyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TuxiftyApp.swift
+//  Tuxifty
+//
+//  Created by Ewan Bigotte on 01/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct TuxiftyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
